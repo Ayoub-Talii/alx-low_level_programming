1275529ba0281df6e6bd74b3178006a933f0084d
Ayoub Talii <ayoubtalii04.com>
2023-06-15 05:15:17 -0700

    1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints "Programming is like building a multilingual puzzle, followed by a newline"
+ * return 0.
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	
+	return(0);
 }
